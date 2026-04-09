5754ddfc00259a9533a4391dab3de16647527871
Shuham-Aswal <shubhamaswal1976@gmail.com>
2026-04-09 09:24:06 +0530

    shopping-cart

---
+++ class-17/shopping-cart/src/components/Shopping.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Shopping = () => {
+  return (
+
+    
+     <div className="shoppingcart">
+
+
+        <ul>
+            <li>
+                
+                <div className="pname licomp ">
+                    apple
+                </div>
+                <div className="pricing licomp">
+                    200 rupees
+                </div>
+
+                <div className="status licomp">remove/add</div>
+            
+            
+            
+            </li>
+        </ul>
+
+
+
+
+
+
+     </div>
+    
+    
+    
+ 
+  
+  )
+}
+
+export default Shopping
